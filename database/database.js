ea0795c2c896d1635335b9ed63b8d5ad42e6e3a0
Gareth Leake <gfleake@gmail.com>
2019-10-07 20:39:07 -0400

    database is up and running

--- database/database.js
+++ database/database.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose');
+const path = require('path');
+
+require('dotenv').config({
+  path: path.join(__dirname, '../.env')
+});
+
 const dbConnection = process.env.DATABASE_URI;
+console.log('dbConnection: ', dbConnection);
+console.log('mongoose object', mongoose);
 
 mongoose.connect(dbConnection, {
-  dbName: 'CrypTick'
+  dbName: 'CrypTick',
+  useNewUrlParser: true
 })
+.catch(err => console.log(err));
 
 mongoose.connection.on('error', ()=>{
   console.log(error)

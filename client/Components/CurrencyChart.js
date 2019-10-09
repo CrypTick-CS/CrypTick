10dd96a4af4e1f9fb6cb358aae06dc1096f5a7ef
Sam Parsons <samparsons269@gmail.com>
2019-10-09 15:45:31 -0400

    semi working streaming chart

--- client/Components/CurrencyChart.js
+++ client/Components/CurrencyChart.js
@@ -4,7 +4,7 @@ import {
   VerticalGridLines,
   XAxis,
   XYPlot,
-  YAxis, LineMarkSeries
+  YAxis, LineSeries
 } from 'react-vis';
 import config from '../../config';
 import styled from 'styled-components';
@@ -21,18 +21,41 @@ class CurrencyChart extends React.Component {
     super();
     this.state = {
       data: [],
-      intervalID: null
+      intervalID: null,
+      yDomain: null,
     }
   }
 
   componentDidMount() {
+    fetch(`https://min-api.cryptocompare.com/data/v2/histominute?fsym=BTC&tsym=USD&limit=30&toTs=${Date.now()}&api_key=${config.key}`)
+    .then(res => res.json())
+    .then(res => {
+      console.log(res['Data']['Data'])
+      let high = 0;
+      let low = 100000;
+      const data = res['Data']['Data'].slice(-30).map(element => {
+        if (element.close > high) high = element.close
+        if (element.close < low) low = element.close
+        return {x: Number(element.time.toString() + '000'), y: element.close}
+      })
+      console.log(data);
+      this.setState({
+      data,
+        yDomain: [low - 20, high + 20]
+    })
+    });
     const intervalID = setInterval(() => {
       fetch(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD&api_key=${config.key}`).then(res => res.json())
       .then(res => {
+        const low = this.state.data.reduce((acc, cur) => acc > cur.y ? y : acc)
+        console.log(low);
+        const high = this.state.data.reduce((acc, cur) => acc < cur.y ? y : acc)
+        console.log(high);
         this.setState({
-          data: this.state.data.concat({x: Date.now(), y: res['BTC']['USD']}).slice(-30)})
-      })
-      }, 4000);
+          data: this.state.data.concat({x: Date.now(), y: res['BTC']['USD']})
+        })
+      }, console.log(this.state.data))
+      }, 1000);
 
     this.setState({intervalID});
   }
@@ -44,12 +67,12 @@ class CurrencyChart extends React.Component {
   render() {
     return (
       <CurrencyChartDiv>
-        <XYPlot height={600} width={600}>
+        <XYPlot height={600} width={600} yDomain={this.state.yDomain}>
           <VerticalGridLines />
           <HorizontalGridLines />
           <XAxis />
           <YAxis />
-          <LineMarkSeries 
+          <LineSeries 
             style={{
               strokeLinejoin: 'round',
               strokeWidth: 4
